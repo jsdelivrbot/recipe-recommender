aabfa70da68adc3b9b9048ab7260c26c24d678a8
Alex Syverson <alex.syverson@gmail.com>
2017-05-24 21:01:24 -0700

    add server auth

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
+import Home from './components/home';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Home} />
+      </Switch>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
